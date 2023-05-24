88560261560d0988a38ca810f00db6b658f0c922
Xiwang <thegostdev@outlook.com>
2023-05-24 17:42:21 +0200

    ajustement css pas beau

--- React/PenalityBox/navigation/screens/Home.js
+++ React/PenalityBox/navigation/screens/Home.js
@@ -1,11 +1,23 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen({navigation}) {
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={styles.homeContainer}>
             <Text onPress={() => alert('This is the "Home" screen.')}
-            style={{ fontSize: 26, fontWeight: "bold" }}>Home Screen</Text>
+            style={styles.homeTitle}>Home Screen</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    homeContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    homeTitle: {
+        fontSize: 26,
+        fontWeight: "bold",
+    },
+});
