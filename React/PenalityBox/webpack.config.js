1bbcd1a82411fc94d93799a8d99fb025a178255a
Xiwang <thegostdev@outlook.com>
2023-07-05 16:51:58 +0200

    webpack

--- React/PenalityBox/webpack.config.js
+++ React/PenalityBox/webpack.config.js
@@ -3,26 +3,25 @@ const createExpoWebpackConfig = require("@expo/webpack-config");
 
 module.exports = function (env, argv) {
   env.mode = "development";
- const config = createExpoWebpackConfig(
+  const config = createExpoWebpackConfig(
     {
       ...env,
-      output: {
-        publicPath: "/penalitybox/",
-        publicUrl: "/penalitybox/",
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            use: {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "static/media",
-              },
+      modifyWebpackConfig: (expoConfig) => {
+        expoConfig.module.rules.push({
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "static/media",
+              publicPath: "/penalitybox/static/media",
             },
           },
-        ],
+        });
+
+        expoConfig.output.publicPath = "/penalitybox/";
+
+        return expoConfig;
       },
     },
     argv
